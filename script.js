6f32af6e694a9ece93e9754c1392fb97ae7fd86c
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2022-12-01 15:35:34 +0100

    remise à zero html/css/js pour fisrt mobile

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// lorsque on clique sur menu_container alors on affiche notre menu
 
+let burger = document.querySelector('.menu_container');
+let menu = document.querySelector('.nav_menu');
+
+burger.addEventListener('click', function(){
+    menu.classList.toggle('toggle');
+});
