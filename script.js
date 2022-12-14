4acb2e9c95c641b9c188760ed385b7e0b124deb0
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2022-12-14 22:16:45 +0100

    changement slide grand ecran nickel

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-// pour rendre ma nav active
+ // pour rendre ma nav active  1er test pour mon menu qui fonctionne bien
 
-//1. je crée une variable pour l'icone burger en recuperant le nom de la classe de mon html qui est nav-toogle
+// //1. je crée une variable pour l'icone burger en recuperant le nom de la classe de mon html qui est nav-toogle
 
  let iconeBurger = document.querySelector(".nav-toogle");
 
  //2. je crée une variable navigation en recuperant la class nav menu
 
- let navigation = document.querySelector("nav");
+  let navigation = document.querySelector("nav");
 
 
- //3. j'ajoute un evenement quand on clic sur les 3 barres. 
- iconeBurger.addEventListener("click",toogleNav);
+ //3. j'ajoute un evenement quand on clic sur les 3 barres.pour ecouter un événement.
+  iconeBurger.addEventListener("click",toogleNav);
 
- //4.
+  //4.
 
  let lienNav = document.querySelectorAll("a");
  console.log(lienNav);
 
 
- //4. je crée la fonction de toogleNav que j'ai mis au dessus
- // et j'ajoute la fonction pour la navigation en fonction qu'on clic sur le menu
+  //4. je crée la fonction de toogleNav que j'ai mis au dessus
+  // et j'ajoute la fonction pour la navigation en fonction qu'on clic sur le menu 
  
- function toogleNav(){
-    iconeBurger.classList.toggle("active")
+  function toogleNav(){
+     iconeBurger.classList.toggle("active")
     navigation.classList.toggle("nav-taille") 
-    navigation.style.transition ='all ease 1s';
-            
+     navigation.style.transition ='all ease 1s';
+          
     for(let i=0;i < lienNav.length;i++){
-lienNav[i].classList.toggle('ecriture')
-lienNav[i].style.transition ='all ease 1s';
+ lienNav[i].classList.toggle('ecriture')
+ lienNav[i].style.transition ='all ease 1s';
     }
- }
+  }
+
+
+ 
+
 
  //5. je veux changer la couleur de mon titre Sandra Conil
  let titreSc = document.querySelector("#accueil");
@@ -45,9 +49,6 @@ titreSc.addEventListener('mouseleave',function(){
 titreSc.style.color ="white";
 });
 
-
-
-//compteur visiteurs
 
 
 
